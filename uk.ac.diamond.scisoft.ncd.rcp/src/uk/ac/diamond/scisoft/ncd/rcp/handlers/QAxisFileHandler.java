195738604c48e3813d0d653d4adbee14197b302c
Irakli Sikharulidze <irakli.sikharulidze@diamond.ac.uk>
2012-06-27 16:14:13 +0100

    SCISOFT NCD: remove dependency on CALIBRATIONFUNCTIONNCD GuiBean

--- uk.ac.diamond.scisoft.ncd.rcp/src/uk/ac/diamond/scisoft/ncd/rcp/handlers/QAxisFileHandler.java
+++ uk.ac.diamond.scisoft.ncd.rcp/src/uk/ac/diamond/scisoft/ncd/rcp/handlers/QAxisFileHandler.java
@@ -185,9 +185,6 @@ public class QAxisFileHandler extends AbstractHandler {
 					IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
 					IViewPart activePlot = page.findView(PlotView.ID + "DP");
 					if (activePlot instanceof PlotView) {
-						if (crb != null)
-							((PlotView) activePlot).putGUIInfo(GuiParameters.CALIBRATIONFUNCTIONNCD, crb);
-
 						AbstractPlottingSystem plotSystem = PlottingFactory.getPlottingSystem("Dataset Plot");
 						plotSystem.setDefaultPlotType(PlotType.IMAGE);
 						IRegion sector = plotSystem.getRegion("Calibration");
