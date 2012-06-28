ea3747cdbf7d9c8978dad8376a91b86d119e1920
Irakli Sikharulidze <irakli.sikharulidze@diamond.ac.uk>
2012-06-28 10:29:02 +0100

    SCISOFT NCD: check for detector data presence in detector response file selection handler

--- uk.ac.diamond.scisoft.ncd.rcp/src/uk/ac/diamond/scisoft/ncd/rcp/handlers/DetectorResponseFileHandler.java
+++ uk.ac.diamond.scisoft.ncd.rcp/src/uk/ac/diamond/scisoft/ncd/rcp/handlers/DetectorResponseFileHandler.java
@@ -24,39 +24,67 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.jface.dialogs.ErrorDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.osgi.util.NLS;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.ui.services.ISourceProviderService;
+import org.eclipse.ui.statushandlers.StatusManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import uk.ac.diamond.scisoft.analysis.hdf5.HDF5File;
+import uk.ac.diamond.scisoft.analysis.hdf5.HDF5NodeLink;
+import uk.ac.diamond.scisoft.analysis.io.HDF5Loader;
+import uk.ac.diamond.scisoft.ncd.preferences.NcdMessages;
 import uk.ac.diamond.scisoft.ncd.rcp.NcdPerspective;
 import uk.ac.diamond.scisoft.ncd.rcp.NcdProcessingSourceProvider;
 
 public class DetectorResponseFileHandler extends AbstractHandler {
 
+	private static final Logger logger = LoggerFactory.getLogger(DetectorResponseFileHandler.class);
+
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindow(event);
+		ISourceProviderService service = (ISourceProviderService) window.getService(ISourceProviderService.class);
+		NcdProcessingSourceProvider ncdSaxsDetectorSourceProvider = (NcdProcessingSourceProvider) service
+				.getSourceProvider(NcdProcessingSourceProvider.SAXSDETECTOR_STATE);
+		NcdProcessingSourceProvider ncdDrFileSourceProvider = (NcdProcessingSourceProvider) service
+				.getSourceProvider(NcdProcessingSourceProvider.DRFILE_STATE);
+
 		final ISelection selection = HandlerUtil.getCurrentSelection(event);
 		if (selection instanceof IStructuredSelection) {
-			final IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindow(event);
-			if (((IStructuredSelection)selection).toList().size() == 1) {
-				ISourceProviderService service = (ISourceProviderService) window.getService(ISourceProviderService.class);
-				NcdProcessingSourceProvider ncdDrFileSourceProvider = (NcdProcessingSourceProvider) service.getSourceProvider(NcdProcessingSourceProvider.DRFILE_STATE);
-				String fileName;
-				if (((IStructuredSelection)selection).getFirstElement() instanceof IFile)
-					fileName = ((IFile)((IStructuredSelection)selection).getFirstElement()).getLocation().toString();
-				else 
-					fileName = ((File)((IStructuredSelection)selection).getFirstElement()).getAbsolutePath();
+			String detectorSaxs = ncdSaxsDetectorSourceProvider.getSaxsDetector();
+			if (detectorSaxs == null)
+				return ErrorDialog(NcdMessages.NO_SAXS_DETECTOR, null);
+
+			String fileName;
+			if (((IStructuredSelection) selection).getFirstElement() instanceof IFile)
+				fileName = ((IFile) ((IStructuredSelection) selection).getFirstElement()).getLocation().toString();
+			else
+				fileName = ((File) ((IStructuredSelection) selection).getFirstElement()).getAbsolutePath();
+
+			try {
+				HDF5File bgFile = new HDF5Loader(fileName).loadTree();
+				HDF5NodeLink node = bgFile.findNodeLink("/entry1/" + detectorSaxs + "/data");
+				if (node == null) {
+					return ErrorDialog(NLS.bind(NcdMessages.NO_DR_DATA, fileName), null);
+				}
+
 				ncdDrFileSourceProvider.setDrFile(fileName);
-			} else {
-				Status status = new Status(IStatus.ERROR, NcdPerspective.PLUGIN_ID, "Only single file can be used as a reference for detector response procedure.");
-				ErrorDialog.openError(window.getShell(), "Detector response file selection error", "Please select one data file.", status);
+			} catch (Exception e) {
+				return ErrorDialog(NLS.bind(NcdMessages.NO_DR_DATA, fileName), e);
 			}
-			
 		}
 		return null;
 	}
 
+	private IStatus ErrorDialog(String msg, Exception e) {
+		logger.error(msg, e);
+		Status status = new Status(IStatus.ERROR, NcdPerspective.PLUGIN_ID, msg, e);
+		StatusManager.getManager().handle(status, StatusManager.SHOW);
+		return null;
+	}
 }
