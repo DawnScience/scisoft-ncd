18e4823ceb6b43e906cb66612f3590aa7924cb80
Irakli Sikharulidze <irakli.sikharulidze@diamond.ac.uk>
2012-07-19 17:19:50 +0100

    SCISOFT NCD: remove warning

--- uk.ac.diamond.scisoft.ncd.rcp/src/uk/ac/diamond/scisoft/ncd/rcp/views/NcdQAxisCalibration.java
+++ uk.ac.diamond.scisoft.ncd.rcp/src/uk/ac/diamond/scisoft/ncd/rcp/views/NcdQAxisCalibration.java
@@ -55,7 +55,6 @@ import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
