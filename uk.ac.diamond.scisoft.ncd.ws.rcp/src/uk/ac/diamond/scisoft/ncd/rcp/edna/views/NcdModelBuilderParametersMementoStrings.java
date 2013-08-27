e474d827c97aa5b629747772d60d3dbe89c37885
Jun Aishima <jun.aishima@diamond.ac.uk>
2013-08-27 15:47:10 +0100

    SCI-794 create strings class for storing memento

---
+++ uk.ac.diamond.scisoft.ncd.ws.rcp/src/uk/ac/diamond/scisoft/ncd/rcp/edna/views/NcdModelBuilderParametersMementoStrings.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2013 Diamond Light Source Ltd.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.ac.diamond.scisoft.ncd.rcp.edna.views;
+
+public class NcdModelBuilderParametersMementoStrings {
+	public static final String BIOSAXS_DATA_FILE = "biosaxs.dataFile";
+	public static final String BIOSAXS_WORKING_DIRECTORY = "biosaxs.working.directory";
+	public static final String BIOSAXS_RESULTS_DIRECTORY = "biosaxs.results.directory";
+	public static final String BIOSAXS_PATH_TO_Q = "biosaxs.pathToQ";
+	public static final String BIOSAXS_PATH_TO_DATA = "biosaxs.pathToData";
+	public static final String BIOSAXS_NUMBER_OF_FRAMES = "biosaxs.numberFrames";
+	public static final String BIOSAXS_Q_MIN = "biosaxs.qMin";
+	public static final String BIOSAXS_Q_MIN_INVERSE_ANGSTROM_UNITS = "biosaxs.qMinInverseAngstromUnits";
+	public static final String BIOSAXS_Q_MAX = "biosaxs.qMax";
+	public static final String BIOSAXS_Q_MAX_INVERSE_ANGSTROM_UNITS = "biosaxs.qMaxInverseAngstromUnits";
+	public static final String BIOSAXS_FIRST_POINT = "biosaxs.first.point";
+	public static final String BIOSAXS_LAST_POINT = "biosaxs.last.point";
+	public static final String BIOSAXS_NUMBER_THREADS = "biosaxs.number.threads";
+	public static final String BIOSAXS_GNOM_ONLY = "biosaxs.gnom.only";
+	public static final String BIOSAXS_GNOM_MIN_DISTANCE = "biosaxs.gnom.min.distance";
+	public static final String BIOSAXS_GNOM_MIN_DISTANCE_ANGSTROM_UNITS = "biosaxs.gnom.min.distance.angstrom.units";
+	public static final String BIOSAXS_GNOM_MAX_DISTANCE = "biosaxs.gnom.max.distance";
+	public static final String BIOSAXS_GNOM_MAX_DISTANCE_ANGSTROM_UNITS = "biosaxs.gnom.max.distance.angstrom.units";
+	public static final String BIOSAXS_GNOM_NUMBER_SEARCH = "biosaxs.gnom.number.search";
+	public static final String BIOSAXS_GNOM_TOLERANCE = "biosaxs.gnom.tolerance";
+	public static final String BIOSAXS_DAMMIF_SYMMETRY_INDEX = "biosaxs.dammif.symmetry.index";
+	public static final String BIOSAXS_DAMMIF_FAST = "biosaxs.dammif.fast";
+}
