7de60fb2084e386fdbfb61fd79e433806e42ca62
matthew.gerring <matthew.gerring@gmail.com>
2014-01-09 12:07:54 +0000

    Added ability to make a data project in case test run outside DAWN (like GDA for instance).

--- uk.ac.diamond.scisoft.ncd.actors/test/uk/ac/diamond/scisoft/ncd/actors/test/DataReductionPipelineTest.java
+++ uk.ac.diamond.scisoft.ncd.actors/test/uk/ac/diamond/scisoft/ncd/actors/test/DataReductionPipelineTest.java
@@ -14,6 +14,8 @@ import java.io.FileInputStream;
 import java.util.Arrays;
 import java.util.List;
 
+import org.dawb.common.util.eclipse.BundleUtils;
+import org.dawb.common.util.io.FileUtils;
 import org.dawb.common.util.io.IFileUtils;
 import org.dawb.common.util.test.TestUtils;
 import org.dawb.hdf5.HierarchicalDataFactory;
@@ -23,15 +25,21 @@ import org.dawb.passerelle.common.utils.ModelUtils;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Platform;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.osgi.framework.Bundle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -57,8 +65,22 @@ public class DataReductionPipelineTest {
 
 		// We make some copies in the results folder and test data sets produced by these workflows.
 		final IProject workflows = ResourcesPlugin.getWorkspace().getRoot().getProject("workflows");
-		final IFolder  res  = workflows.getFolder("data").getFolder("results");
-	    if (!res.exists()) throw new Exception("folder "+res.getName()+" must exist!");
+		IFolder  res  = workflows.getFolder("data").getFolder("results");
+	    if (res==null || !res.exists()) {
+			final Bundle bundle = Platform.getBundle("org.dawb.workbench.examples");
+            if (bundle==null) throw new Exception("Examples not found!");
+			
+    		final IWorkspaceRoot root         = ResourcesPlugin.getWorkspace().getRoot();
+			createDataProject("data", root, true, new NullProgressMonitor());
+			
+			IWorkspace ws = ResourcesPlugin.getWorkspace();
+			ws.save(false, new NullProgressMonitor());
+			
+			res  = workflows.getFolder("data").getFolder("results");
+	    	
+	    }
+	   
+	    if (res==null || !res.exists()) throw new Exception("folder "+res.getName()+" must exist!");
 	    final IFile    toCopy=res.getFile("billeA_4201_EF_XRD_5998.edf");   		 		
 	    
 	    // 10 copies
@@ -221,4 +243,71 @@ public class DataReductionPipelineTest {
 	public static void after() {
 		WorkbenchServiceManager.stopWorkbenchService();
 	}
+	
+    /**
+     * 
+     * @param name
+     * @param root
+     * @param mon
+     * @return
+     * @throws Exception
+     */
+	public static IProject createDataProject(final String           name, 
+			                                 final IWorkspaceRoot   root,
+			                                 final boolean          createExamples,
+			                                 final IProgressMonitor mon) throws Exception {
+		
+		if (root.getProject(name).exists()) return root.getProject(name);
+
+		final IProject data = root.getProject(name);
+		data.create(mon);
+		data.open(mon);
+
+		if (createExamples) {
+	        final IFolder examples = data.getFolder("examples");
+			examples.create(true, true, mon);
+			
+			// We copy all the data from examples here.
+			// Use bundle as works even in debug mode.
+	        final File examplesDir = BundleUtils.getBundleLocation("org.dawb.workbench.examples");
+	        final File dataDir     = new File(examplesDir, "data");
+	        logger.debug("Using data folder "+dataDir.getAbsolutePath());
+	        if (dataDir.exists()) {
+	        	FileUtils.recursiveCopy(dataDir, new File(examples.getLocation().toOSString()));
+	        }
+	        
+	        final IFolder src = data.getFolder("src");
+	        src.create(true, true, mon);
+
+	        final File pythonDir     = new File(examplesDir, "python");
+	        if (pythonDir.exists()) {
+	        	FileUtils.recursiveCopy(pythonDir, new File(src.getLocation().toOSString()));
+	        }
+
+		}
+	        
+        addDataNature(data, mon);
+        
+        data.refreshLocal(IResource.DEPTH_INFINITE, mon);
+
+        return data;
+	}
+
+	
+	/**
+	 * 
+	 * @param workflows
+	 * @param mon
+	 * @throws CoreException 
+	 */
+	private static void addDataNature(final IProject data,
+			                          final IProgressMonitor mon) throws CoreException {
+		
+		IProjectDescription description = data.getDescription();
+		description.setNatureIds(new String[]{"org.dawb.common.ui.DataNature"});
+		data.setDescription(description, mon);
+		
+	}
+
+
 }
