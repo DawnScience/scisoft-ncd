202d0179facfcf2f499296e89ccec99b3e7da18b
Jun Aishima <jun.aishima@diamond.ac.uk>
2014-08-22 09:19:28 +0100

    move launcher, test, and data into ncd.actors.test plugin
     * also move data into data directory

--- uk.ac.diamond.scisoft.ncd.actors.test/src/uk/ac/diamond/scisoft/ncd/actors/DataReductionPipelineTest.java
+++ uk.ac.diamond.scisoft.ncd.actors.test/src/uk/ac/diamond/scisoft/ncd/actors/DataReductionPipelineTest.java
@@ -7,7 +7,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */ 
-package uk.ac.diamond.scisoft.ncd.actors.test;
+package uk.ac.diamond.scisoft.ncd.actors;
 
 import java.io.File;
 import java.io.FileInputStream;
