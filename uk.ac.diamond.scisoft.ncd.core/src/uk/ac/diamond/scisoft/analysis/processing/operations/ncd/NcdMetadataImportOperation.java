52224e200622405ca30cf82ffbf264e70dcc1d06
Jun Aishima <jun.aishima@diamond.ac.uk>
2014-10-17 16:36:01 +0100

    add operation to current GUI

--- uk.ac.diamond.scisoft.ncd.core/src/uk/ac/diamond/scisoft/analysis/processing/operations/ncd/NcdMetadataImportOperation.java
+++ uk.ac.diamond.scisoft.ncd.core/src/uk/ac/diamond/scisoft/analysis/processing/operations/ncd/NcdMetadataImportOperation.java
@@ -1,1 +1,36 @@
+/*
+ * Copyright (c) 2014 Diamond Light Source Ltd.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
 package uk.ac.diamond.scisoft.analysis.processing.operations.ncd;
+
+import org.eclipse.dawnsci.analysis.api.processing.AbstractOperation;
+import org.eclipse.dawnsci.analysis.api.processing.OperationData;
+import org.eclipse.dawnsci.analysis.api.processing.OperationRank;
+
+public class NcdMetadataImportOperation extends AbstractOperation<NormalisationModel, OperationData> {
+
+	@Override
+	public String getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public OperationRank getInputRank() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public OperationRank getOutputRank() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
