625ad16782729aa9ace75773e0b17058e4afa6b3
Jun Aishima <jun.aishima@diamond.ac.uk>
2014-10-22 11:50:06 +0100

    call DetectorResponse, handle input/output

--- uk.ac.diamond.scisoft.ncd.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/ncd/NcdDetectorResponseOperation.java
+++ uk.ac.diamond.scisoft.ncd.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/ncd/NcdDetectorResponseOperation.java
@@ -9,9 +9,18 @@
 
 package uk.ac.diamond.scisoft.analysis.processing.operations.ncd;
 
+import org.eclipse.dawnsci.analysis.api.dataset.IDataset;
+import org.eclipse.dawnsci.analysis.api.dataset.Slice;
+import org.eclipse.dawnsci.analysis.api.monitor.IMonitor;
 import org.eclipse.dawnsci.analysis.api.processing.AbstractOperation;
 import org.eclipse.dawnsci.analysis.api.processing.OperationData;
+import org.eclipse.dawnsci.analysis.api.processing.OperationException;
 import org.eclipse.dawnsci.analysis.api.processing.OperationRank;
+import org.eclipse.dawnsci.analysis.dataset.impl.Dataset;
+import org.eclipse.dawnsci.analysis.dataset.impl.DoubleDataset;
+import org.eclipse.dawnsci.analysis.dataset.impl.FloatDataset;
+
+import uk.ac.diamond.scisoft.ncd.core.DetectorResponse;
 
 public class NcdDetectorResponseOperation extends AbstractOperation<NcdDetectorResponseModel, OperationData> {
 
@@ -28,5 +37,24 @@ public class NcdDetectorResponseOperation extends AbstractOperation<NcdDetectorR
 	@Override
 	public OperationRank getOutputRank() {
 		return OperationRank.SAME;
+	}
+	
+	@Override
+	public OperationData execute(IDataset slice, IMonitor monitor) throws OperationException {
+		DetectorResponse response = new DetectorResponse();
+		response.setResponse(model.getResponse());
+		FloatDataset errors = (FloatDataset) slice.getError();
+		FloatDataset data = (FloatDataset) slice.getSlice(new Slice());
+		int[] flatShape = data.getShape();
+
+		Object[] detData = response.process(data.getData(), errors.getData(), flatShape[0], flatShape);
+		OperationData toReturn = new OperationData();
+		float[] mydata = (float[]) detData[0];
+		double[] myerrors = (double[]) detData[1];
+
+		Dataset myres = new FloatDataset(mydata, slice.getShape());
+		myres.setErrorBuffer(new DoubleDataset(myerrors, slice.getShape()));
+		toReturn.setData(myres);
+		return toReturn;
 	}
 }
