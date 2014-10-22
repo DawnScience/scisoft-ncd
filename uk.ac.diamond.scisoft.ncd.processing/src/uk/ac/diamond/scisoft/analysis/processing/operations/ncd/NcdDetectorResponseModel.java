f1c0d327d34a8d275406d9168043761c1fd2bd7d
Jun Aishima <jun.aishima@diamond.ac.uk>
2014-10-22 12:06:09 +0100

    parameter should be filepath, which can be used to get response

--- uk.ac.diamond.scisoft.ncd.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/ncd/NcdDetectorResponseModel.java
+++ uk.ac.diamond.scisoft.ncd.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/ncd/NcdDetectorResponseModel.java
@@ -1,20 +1,20 @@
 package uk.ac.diamond.scisoft.analysis.processing.operations.ncd;
 
 import org.eclipse.dawnsci.analysis.api.processing.model.AbstractOperationModel;
+import org.eclipse.dawnsci.analysis.api.processing.model.FileType;
 import org.eclipse.dawnsci.analysis.api.processing.model.OperationModelField;
-import org.eclipse.dawnsci.analysis.dataset.impl.Dataset;
 
 public class NcdDetectorResponseModel extends AbstractOperationModel {
 
-	@OperationModelField(hint="File containing detector response",label = "Response file" )
-	public Dataset response;
+	@OperationModelField(hint="File containing detector response",file = FileType.EXISTING_FILE, label = "Response file" )
+	private String filePath = "";
 
-	public Dataset getResponse() {
-		return response;
+	public String getFilePath() {
+		return filePath;
 	}
 
-	public void setResponse(Dataset response) {
-		this.response = response;
+	public void setFilePath(String filePath) {
+		firePropertyChange("filePath", this.filePath, this.filePath = filePath);
 	}
 
 }
