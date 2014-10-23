75f1464537c3d26cea9092c3ee6ada8d39352107
Jun Aishima <jun.aishima@diamond.ac.uk>
2014-10-23 15:37:28 +0100

    add NCD Sector Integration operation

---
+++ uk.ac.diamond.scisoft.ncd.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/ncd/NcdSectorIntegrationModel.java
@@ -0,0 +1,7 @@
+package uk.ac.diamond.scisoft.analysis.processing.operations.ncd;
+
+import uk.ac.diamond.scisoft.analysis.processing.operations.IntegrationModel;
+
+public class NcdSectorIntegrationModel extends IntegrationModel {
+
+}
