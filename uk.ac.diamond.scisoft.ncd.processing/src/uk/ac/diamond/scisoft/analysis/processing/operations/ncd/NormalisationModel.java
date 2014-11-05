f95c91aaf52b638e0acd1ccbdd8a22e27a466c5a
Jun Aishima <jun.aishima@diamond.ac.uk>
2014-11-05 16:12:44 +0000

    allow max normalization value to be 1e10
     * looks like the NCD Data Reduction pipeline used 1e10 to get the
       values back into a more normal range instead of 1e-11

--- uk.ac.diamond.scisoft.ncd.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/ncd/NormalisationModel.java
+++ uk.ac.diamond.scisoft.ncd.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/ncd/NormalisationModel.java
@@ -14,7 +14,7 @@ import org.eclipse.dawnsci.analysis.api.processing.model.FileType;
 import org.eclipse.dawnsci.analysis.api.processing.model.OperationModelField;
 
 public class NormalisationModel extends AbstractOperationModel {
-	@OperationModelField(hint="Normalisation scaling value",label = "Normalisation value" )
+	@OperationModelField(hint="Normalisation scaling value",max=1e10,label = "Normalisation value" )
 	private double normValue = 1;
 
 	@OperationModelField(hint="Calibration channel location in scan data",label = "Calibration channel number" )
