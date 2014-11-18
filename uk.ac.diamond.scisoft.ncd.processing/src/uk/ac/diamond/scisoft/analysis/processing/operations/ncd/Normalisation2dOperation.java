0d8eba134a3bc45ce99bb183cdeab27a56cc9ca3
Jun Aishima <jun.aishima@diamond.ac.uk>
2014-11-18 16:31:13 +0000

    add 2-d version of normalization
     * same code, just different rank settings

---
+++ uk.ac.diamond.scisoft.ncd.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/ncd/Normalisation2dOperation.java
@@ -0,0 +1,21 @@
+package uk.ac.diamond.scisoft.analysis.processing.operations.ncd;
+
+import org.eclipse.dawnsci.analysis.api.processing.OperationRank;
+
+public class Normalisation2dOperation extends NormalisationOperation {
+
+	@Override
+	public String getId() {
+		return "uk.ac.diamond.scisoft.analysis.processing.Normalisation2d";
+	}
+
+	@Override
+	public OperationRank getInputRank() {
+		return OperationRank.TWO;
+	}
+
+	@Override
+	public OperationRank getOutputRank() {
+		return OperationRank.TWO;
+	}
+}
