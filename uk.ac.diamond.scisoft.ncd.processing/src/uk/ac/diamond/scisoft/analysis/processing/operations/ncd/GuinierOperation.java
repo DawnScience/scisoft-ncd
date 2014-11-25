8135920c4ccac91bd40c4175a71879a31e9ffbe6
Jun Aishima <jun.aishima@diamond.ac.uk>
2014-11-25 16:44:01 +0000

    use the correct model

--- uk.ac.diamond.scisoft.ncd.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/ncd/GuinierOperation.java
+++ uk.ac.diamond.scisoft.ncd.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/ncd/GuinierOperation.java
@@ -11,12 +11,12 @@ import org.eclipse.dawnsci.analysis.api.processing.AbstractOperation;
 import org.eclipse.dawnsci.analysis.api.processing.OperationData;
 import org.eclipse.dawnsci.analysis.api.processing.OperationException;
 import org.eclipse.dawnsci.analysis.api.processing.OperationRank;
-import org.eclipse.dawnsci.analysis.api.processing.model.AbstractOperationModel;
 import org.eclipse.dawnsci.analysis.dataset.impl.DoubleDataset;
 
+import uk.ac.diamond.scisoft.analysis.processing.operations.EmptyModel;
 import uk.ac.diamond.scisoft.ncd.core.data.plots.GuinierPlotData;
 
-public class GuinierOperation extends AbstractOperation<AbstractOperationModel, OperationData> {
+public class GuinierOperation extends AbstractOperation<EmptyModel, OperationData> {
 
 	@Override
 	public String getId() {
