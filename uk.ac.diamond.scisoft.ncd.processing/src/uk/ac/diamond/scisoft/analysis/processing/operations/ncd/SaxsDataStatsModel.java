ae4086a84c3bf2b3a750eeb276e96d2f9580a8e4
Jun Aishima <jun.aishima@diamond.ac.uk>
2014-11-26 13:00:13 +0000

    create operation to filter data

---
+++ uk.ac.diamond.scisoft.ncd.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/ncd/SaxsDataStatsModel.java
@@ -0,0 +1,7 @@
+package uk.ac.diamond.scisoft.analysis.processing.operations.ncd;
+
+import org.eclipse.dawnsci.analysis.api.processing.model.AbstractOperationModel;
+
+public class SaxsDataStatsModel extends AbstractOperationModel {
+	
+}
