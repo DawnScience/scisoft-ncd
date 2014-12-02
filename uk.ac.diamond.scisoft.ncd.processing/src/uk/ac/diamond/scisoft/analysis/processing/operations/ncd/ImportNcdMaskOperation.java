c23f5ea699e744703c9632fff259c57927381c7b
matthew.gerring <matthew.gerring@gmail.com>
2014-12-02 08:50:17 +0000

    Fix to method override.

--- uk.ac.diamond.scisoft.ncd.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/ncd/ImportNcdMaskOperation.java
+++ uk.ac.diamond.scisoft.ncd.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/ncd/ImportNcdMaskOperation.java
@@ -20,6 +20,7 @@ import org.eclipse.dawnsci.analysis.api.monitor.IMonitor;
 import org.eclipse.dawnsci.analysis.api.processing.OperationData;
 import org.eclipse.dawnsci.analysis.api.processing.OperationException;
 import org.eclipse.dawnsci.analysis.api.processing.OperationRank;
+import org.eclipse.dawnsci.analysis.api.processing.model.IOperationModel;
 import org.eclipse.dawnsci.analysis.dataset.impl.Dataset;
 import org.eclipse.dawnsci.analysis.dataset.impl.DatasetUtils;
 import org.eclipse.dawnsci.hdf5.HierarchicalDataFactory;
@@ -124,8 +125,7 @@ public class ImportNcdMaskOperation extends ImportMaskOperation<ImportMaskModel>
 	}
 	
 	@Override
-	public Class<?> getModelClass() {
-
+	public Class<? extends IOperationModel> getModelClass() {
 		return ImportMaskModel.class;
 	}
 }
