53a7e9e052a13fd905908c2ce67945be0073f512
Jun Aishima <jun.aishima@diamond.ac.uk>
2015-01-23 11:52:47 +0000

    remove dataset selection, use accumulated data to export
     * the conversion service expects a whole file or dataset
     * should be possible to provide option to output individual datasets
       if that would be more useful?
    
    Conflicts:
    	uk.ac.diamond.scisoft.ncd.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/ncd/ExportNcd1dOperation.java

--- uk.ac.diamond.scisoft.ncd.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/ncd/ExportNcd1dOperation.java
+++ uk.ac.diamond.scisoft.ncd.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/ncd/ExportNcd1dOperation.java
@@ -9,16 +9,29 @@
 
 package uk.ac.diamond.scisoft.analysis.processing.operations.ncd;
 
+import org.dawb.common.services.ServiceManager;
+import org.dawb.common.services.conversion.IConversionContext;
+import org.dawb.common.services.conversion.IConversionContext.ConversionScheme;
+import org.dawb.common.services.conversion.IConversionService;
 import org.eclipse.dawnsci.analysis.api.dataset.IDataset;
+import org.eclipse.dawnsci.analysis.api.dataset.ILazyDataset;
 import org.eclipse.dawnsci.analysis.api.monitor.IMonitor;
 import org.eclipse.dawnsci.analysis.api.processing.IExportOperation;
 import org.eclipse.dawnsci.analysis.api.processing.OperationData;
 import org.eclipse.dawnsci.analysis.api.processing.OperationException;
 import org.eclipse.dawnsci.analysis.api.processing.OperationRank;
+import org.eclipse.dawnsci.analysis.dataset.impl.AggregateDataset;
+import org.eclipse.dawnsci.analysis.dataset.impl.Dataset;
+import org.eclipse.dawnsci.analysis.dataset.impl.DatasetUtils;
 import org.eclipse.dawnsci.analysis.dataset.operations.AbstractOperation;
 import org.eclipse.dawnsci.analysis.dataset.slicer.SliceFromSeriesMetadata;
 
+@SuppressWarnings("deprecation")
 public class ExportNcd1dOperation extends AbstractOperation<ExportNcd1dModel, OperationData> implements IExportOperation {
+	private Dataset[] sliceData;
+	private ILazyDataset parent;
+	private int counter;
+	
 	@Override
 	public OperationRank getInputRank() {
 		return OperationRank.ONE;
@@ -38,6 +51,50 @@ public class ExportNcd1dOperation extends AbstractOperation<ExportNcd1dModel, Op
 		if (model.getOutputDirectoryPath() == null) throw new OperationException(this, "Output directory not set!");
 		SliceFromSeriesMetadata ssm = getSliceSeriesMetadata(input);
 		if (ssm == null) throw new OperationException(this, "Dataset has not Origin!");
+		
+		if (model.getOutputDirectoryPath() == null || model.getOutputDirectoryPath().isEmpty()) {
+			throw new OperationException(this, "output directory cannot be empty");
+		}
+		if (parent != ssm.getSourceInfo().getParent()) {
+			parent = ssm.getSourceInfo().getParent();
+			sliceData = null;
+			counter = 0;
+		}
+		
+		
+		Dataset d = DatasetUtils.cast(input,Dataset.FLOAT64);
+		
+		if (sliceData == null) {
+			sliceData = new Dataset[ssm.getTotalSlices()];
+			counter = 0;
+		}
+		
+		//first accumulate all data
+		sliceData[counter] = d;
+		counter++;
+				
+		if (counter == ssm.getTotalSlices()) {
+			IConversionService service = null;
+			try {
+				service = (IConversionService)ServiceManager.getService(IConversionService.class, true);
+			} catch (Exception e1) {
+				throw new OperationException(this, e1);
+			}
+
+			IConversionContext context = service.open("/dls/path_to_some_hdf5_file.nxs"); //a dummy file to create context - TODO provide file-free way to create context!
+			AggregateDataset ag = new AggregateDataset(false, sliceData);
+			context.setLazyDataset(ag);
+			context.setDatasetName(model.getDatasetName());
+			context.setOutputPath(model.getOutputDirectoryPath());
+			context.setConversionScheme(ConversionScheme.CUSTOM_NCD);
+			context.setUserObject(model.getExportFormat());
+
+			try {
+				service.process(context);
+			} catch (Exception e) {
+				throw new OperationException(this, e);
+			}
+		}
 		return null;
 
 	}
