b0ce4ecf9ac693c6ed53697b432e2115cdf5d9da
Jun Aishima <jun.aishima@diamond.ac.uk>
2015-02-26 16:51:09 +0000

    make std dev a separate row, save value to memento

--- uk.ac.diamond.scisoft.ncd/src/uk/ac/diamond/scisoft/ncd/preferences/NcdPreferences.java
+++ uk.ac.diamond.scisoft.ncd/src/uk/ac/diamond/scisoft/ncd/preferences/NcdPreferences.java
@@ -40,6 +40,7 @@ public class NcdPreferences {
 	
 	public static final String NCD_SAMPLETHICKNESS = "ncd.samplethickness";
 	public static final String NCD_ABSOLUTESCALE = "ncd.absolutescale";
+	public static final String NCD_ABSOLUTESCALESTDDEV = "ncd.absolutescalestddev";
 	public static final String NCD_USEFORMSAMPLETHICKNESS = "ncd.usefilesamplethickness";
 	public static final String NCD_BACKGROUNDSUBTRACTION = "ncd.backgroundsubtraction";
 	public static final String NCD_BACKGROUNDSCALE =  "ncd.backgroundscale";
