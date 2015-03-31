9c88e7753e8f41ad98eba2005139049f8a33c315
Jun Aishima <jun.aishima@diamond.ac.uk>
2015-03-31 14:23:42 +0100

    add error, source info to dataset to allow averaging to happen
     * fix AveragingOperation ID

--- uk.ac.diamond.scisoft.ncd.processing.test/src/uk/ac/diamond/scisoft/analysis/processing/operations/ncd/AveragingTest.java
+++ uk.ac.diamond.scisoft.ncd.processing.test/src/uk/ac/diamond/scisoft/analysis/processing/operations/ncd/AveragingTest.java
@@ -5,16 +5,14 @@ import org.eclipse.dawnsci.analysis.api.processing.ExecutionType;
 import org.eclipse.dawnsci.analysis.api.processing.IOperation;
 import org.eclipse.dawnsci.analysis.api.processing.IOperationContext;
 import org.eclipse.dawnsci.analysis.api.processing.IOperationService;
-import org.eclipse.dawnsci.analysis.api.roi.IROI;
-import org.eclipse.dawnsci.analysis.dataset.impl.BooleanDataset;
 import org.eclipse.dawnsci.analysis.dataset.impl.Random;
-import org.eclipse.dawnsci.analysis.dataset.roi.SectorROI;
+import org.eclipse.dawnsci.analysis.dataset.slicer.SliceFromSeriesMetadata;
+import org.eclipse.dawnsci.analysis.dataset.slicer.SourceInformation;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import uk.ac.diamond.scisoft.analysis.processing.Activator;
 import uk.ac.diamond.scisoft.analysis.processing.actor.actors.OperationTransformer;
-import uk.ac.diamond.scisoft.analysis.processing.actor.runner.GraphRunner;
 import uk.ac.diamond.scisoft.analysis.processing.runner.OperationRunnerImpl;
 import uk.ac.diamond.scisoft.analysis.processing.runner.SeriesRunner;
 
@@ -30,35 +28,47 @@ public class AveragingTest {
 	@BeforeClass
 	public static void before() throws Exception {
 		service = (IOperationService)Activator.getService(IOperationService.class);
-		
+		service.createOperations(service.getClass().getClassLoader(), "uk.ac.diamond.scisoft.analysis.processing.operations");
+		service.createOperations(service.getClass().getClassLoader(), "uk.ac.diamond.scisoft.analysis.processing.operations.ncd");
+
 		OperationRunnerImpl.setRunner(ExecutionType.SERIES,   new SeriesRunner());
 	
 		OperationTransformer.setOperationService(service);
 		
-		randomDataset = Random.rand(0.0, 1000.0, 24, 1000, 1000);
-
+		int datasetLength = 1000;
+		randomDataset = Random.rand(0.0, 1000.0, 24, datasetLength);
+		randomDataset.setName("random");
+		randomDataset.setError(randomDataset.clone());
+		SourceInformation si = new SourceInformation("filepath", randomDataset.getName(), randomDataset);
+		randomDataset.setMetadata(new SliceFromSeriesMetadata(si));
 	}
 
+	@SuppressWarnings("unchecked")
 	@Test
 	public void testAveragingJake() throws Exception {
 		//compare my averaging with no Rg filtering with Jake's
-		final IOperation averageJake = service.create("uk.ac.diamond.scisoft.analysis.processing.averagingOperation");
+		final IOperation averageJake = service.create("uk.ac.diamond.scisoft.analysis.processing.operations.AveragingOperation");
 		final IOperationContext context = service.createContext();
+
 		context.setData(randomDataset);
 		context.setSlicing("all"); // All 24 images in first dimension.
 		context.setSeries(averageJake);
+		context.setExecutionType(ExecutionType.SERIES);
 		service.execute(context);
 	}
 
+	@SuppressWarnings("unchecked")
 	@Test
 	public void testAveragingJun() throws Exception {
 		//compare my averaging with no Rg filtering with Jake's
 		final IOperation averageJun = service.create("uk.ac.diamond.scisoft.analysis.processing.NcdAveraging");
 		averageJun.setModel(new NcdAveragingModel());
 		final IOperationContext context = service.createContext();
+
 		context.setData(randomDataset);
 		context.setSlicing("all"); // All 24 images in first dimension.
 		context.setSeries(averageJun);
+		context.setExecutionType(ExecutionType.SERIES);
 		service.execute(context);
 	}
 }
