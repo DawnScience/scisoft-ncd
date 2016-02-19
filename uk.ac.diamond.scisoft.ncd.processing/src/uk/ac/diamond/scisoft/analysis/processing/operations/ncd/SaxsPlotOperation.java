ad310c2a0db39758d0e6d90bcd1ebeb5edfcc85d
Tim Spain <timothy.spain@diamond.ac.uk>
2016-02-19 10:40:41 +0000

    Add copyright/license comments to the Saxs plot tool Operation and Model with the correct year.

--- uk.ac.diamond.scisoft.ncd.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/ncd/SaxsPlotOperation.java
+++ uk.ac.diamond.scisoft.ncd.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/ncd/SaxsPlotOperation.java
@@ -1,3 +1,12 @@
+/*-
+ * Copyright 2016 Diamond Light Source Ltd.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
 package uk.ac.diamond.scisoft.analysis.processing.operations.ncd;
 
 import org.eclipse.dawnsci.analysis.api.dataset.IDataset;
@@ -14,6 +23,12 @@ import org.eclipse.dawnsci.analysis.dataset.operations.AbstractOperation;
 
 import uk.ac.diamond.scisoft.ncd.core.data.SaxsAnalysisPlotType;
 
+/**
+ * An Operation to make the various SAXS plots in the Processing perspective
+ * 
+ * @author Timothy Spain, timothy.spain@diamond.ac.uk
+ *
+ */
 public class SaxsPlotOperation extends AbstractOperation<SaxsPlotModel, OperationData> {
 
 	@Override
