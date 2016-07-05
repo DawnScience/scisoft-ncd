beea33234c5660bd9f8635dd535083deb978f41c
Peter Chang <peter.chang@diamond.ac.uk>
2016-07-05 15:29:15 +0100

    Use new cast method

--- uk.ac.diamond.scisoft.ncd.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/ncd/NcdDetectorResponseOperation.java
+++ uk.ac.diamond.scisoft.ncd.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/ncd/NcdDetectorResponseOperation.java
@@ -16,7 +16,6 @@ import org.eclipse.dawnsci.analysis.api.monitor.IMonitor;
 import org.eclipse.dawnsci.analysis.api.processing.OperationData;
 import org.eclipse.dawnsci.analysis.api.processing.OperationException;
 import org.eclipse.dawnsci.analysis.api.processing.OperationRank;
-import org.eclipse.dawnsci.analysis.dataset.impl.AbstractDataset;
 import org.eclipse.dawnsci.analysis.dataset.impl.Dataset;
 import org.eclipse.dawnsci.analysis.dataset.impl.DatasetFactory;
 import org.eclipse.dawnsci.analysis.dataset.impl.DatasetUtils;
@@ -69,7 +68,7 @@ public class NcdDetectorResponseOperation extends AbstractOperation<NcdDetectorR
 			errors = (FloatDataset) slice.getError();
 		}
 		else {
-			errors = (FloatDataset) data.cast(AbstractDataset.FLOAT32);
+			errors = data.cast(FloatDataset.class);
 		}
 		int[] flatShape = data.getShape();
 
